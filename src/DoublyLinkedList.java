caea07f78acd8c3fdeaeb45b3655f61f02d2485d
EmanuelDellsén <emanuel.dellsen@hotmail.com>
2019-02-23 11:08:58 +0100

    hands on excercise

---
+++ src/DoublyLinkedList.java
@@ -0,0 +1,153 @@
+
+class DoublyLinkedList<Item> {
+
+    private int size = 0;
+    private Node<Item> head;
+    private Node<Item> tail;
+
+    /**
+     * Linked list node helper data type
+     *
+     * @param <Item>
+     */
+    private static class Node<Item> {
+
+        Node<Item> next = null;
+        Item el = null;
+        Node<Item> prev = null;
+
+    }
+
+    /**
+     * Public constructor
+     */
+    public DoublyLinkedList() {
+
+        this.size = size();
+        head.next = tail;
+        tail.prev = head;
+
+        // add things in the constructor to create an empty linked list object.
+    }
+
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Hands on 6, Exercise 1, Get element at index n from the list
+     *
+     * @param n the index of the element to get
+     */
+    public Item get(int n) {
+
+        if ( n > size/2 && n >= 0) {
+
+            Node<Item> currentNode = tail;
+            int counter = size - 1;
+
+            while (currentNode != null) {
+
+                if (counter == n) {
+                    return  currentNode.el;
+                }
+                currentNode = currentNode.prev;
+                counter--;
+            }
+            throw new IllegalArgumentException();
+        }
+        if ( n < size/2 && n >= 0){
+
+            Node<Item> currentNode = head;
+            int counter = 0;
+
+            while ( currentNode != null){
+
+                if (counter == n){
+                    return currentNode.el;
+                }
+                currentNode = currentNode.next;
+                counter++;
+            }
+            throw new IllegalArgumentException();
+
+        }else{
+            throw new IllegalArgumentException();
+
+        }
+    }
+
+    /**
+     * Hands on 6, Exercise 2, Insert element x at index n in the list
+     *
+     * @param n the index where we want to do the insertion
+     * @param x the element to insert
+     */
+  /*  public void insertAt(int n, Item x) {
+
+        Node<Item> newNode = new Node<>();
+        newNode.el = x;
+
+        int temp;
+
+
+        if (n == 0) {
+
+            newNode.next = head;
+            newNode.prev = null;
+            head = newNode;
+
+        } else if ( n == size-1 ){
+
+            newNode.next = null;
+            newNode.prev=tail;
+            tail=newNode;
+
+        } else {
+
+            for (int i = 0; i < size ; i++) {
+
+                if ()
+
+            }
+        }
+
+
+    }*/
+
+    /**
+     * Hands on 6, Exercise 3, Remove the element at index n from the list
+     *
+     * @param n the index where we want to remove an element
+     */
+    public void removeAt(int n) {
+        throw new UnsupportedOperationException();
+
+    }
+
+    /**
+     * Hands on 6, Exercise 4, reverse the list
+     *
+     */
+    public void reverse() {
+
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * return whether the list is empty or not
+     *
+     * @return
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList<Integer> l = new DoublyLinkedList<Integer>();
+
+       // l.insertAt(0, 1);
+
+
+    }
+}
